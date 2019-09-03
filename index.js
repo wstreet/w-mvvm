7257d57c82aec8c85ed146744ea8600d1bb16fa7
wstreet7 <wstreet7@outlook.com>
2019-09-03 23:42:01 +0800

    数据劫持

--- index.js
+++ index.js
@@ -8,5 +8,25 @@ function observe(data) {
 }
 
 function observeProperty(obj, key, value) {
-
+  observe(value)
+  Object.defineProperty(obj, key, {
+    enumerable: true, // 可以枚举
+    configurable: true, // 可以重新定义
+    get() {
+      return value
+    },
+    set(newValue) {
+      if (value === newValue || (newValue !== newValue && value !== value)) {
+        return;
+      }
+      console.log(`${key}更新了, ${value} ==> ${newValue}`)
+      // 可以做通知视图的操作
+      value = newValue
+    }
+  })
 }
+
+const data = {
+  name: 'wstreet'
+}
+observe(data)
