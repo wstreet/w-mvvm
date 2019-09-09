666b74a5921efa7b7b554ed0e9e6cb98faa3e9d4
wstreet7 <wstreet7@outlook.com>
2019-09-09 23:52:24 +0800

    update Compile

--- Compile.js
+++ Compile.js
@@ -48,7 +48,15 @@ class Compile {
 
       if (this.isDirective(attrName)) {
         const exp = attr.value
-        node.innerHTML = typeof this.$val[exp] === 'undefined' ? '' : this.$val[exp]
+        const dir = attrName.substring(2)
+        // 事件指令
+        if (this.isEventDirective) {
+          compileUtil.eventHandler(node, this.$vm, exp, dir)
+        } else {
+          // 普通指令
+          compileUtil[dir] && compileUtil[dir](node, this.$vm, exp)
+        }
+        // node.innerHTML = typeof this.$val[exp] === 'undefined' ? '' : this.$val[exp]
         node.removeAttribute(attrName)
       }
     })
@@ -67,9 +75,81 @@ class Compile {
   isTextNode = (node) => {
     return node.nodeType === 3
   }
-
+  // x-yy指令判断
   isDirective = (attr) => {
     return attr.indexOf('x-') === 0
   }
+
+  // 事件指令判断
+  isEventDirective = (dir) => {
+    return dir.indexOf('on') === 0
+  }
 }
 
+
+
+let $elm
+let timer = null
+
+const compileUtils = {
+  bind(node, vm, exp, dir) {
+    const updateFn = update[`${dir}Updater`]
+    updateFn && updateFn(node, this._getVmVal)
+
+    new Watcher(vm, exp, (value, oldValue) => {
+      updateFn && updateFn(node, value, oldValue)
+    })
+  },
+
+  html(node, vm, exp) {
+    this.bind(node, vm, exp, 'html')
+  },
+
+  text(node, vm, exp) {
+    this.bind(node, vm, exp, 'text')
+  },
+
+  class(node, vm, exp) {
+    this.bind(node, vm, exp, 'class')
+  },
+
+  model(node, vm, exp) {
+    this.bind(node, vm, exp, 'model')
+
+    const self = this
+    const val = this._getVmVal(vm, exp)
+
+    node.addEventListener('input', e => {
+      const newVal = e.target.value
+      $elm = e.target
+
+      if (val === newVal) {
+        return
+      }
+
+      // 设置定时器完成ui异步渲染
+      clearTimeout(timer)
+      timer = setTimeout(() => {
+        self._setVmVal(vm, exp, newVal)
+
+        val = newVal
+      })
+    })
+  },
+
+  // 事件处理
+  eventHandler(node, vm, exp, dir) {
+    const eventType = dir.split(':')[1]
+
+    const fn = vm.$options.methods && vm.$options.methods[exp]
+
+    if (eventType && fn) {
+      node.addEventListener(eventType, fn.bind(vm), false)
+    }
+  },
+
+  _getVmVal(vm, exp) {
+
+  }
+
+}
